e0d7d253bdf9db1234f11849ac57110ed4981079
Muhamad Rian Santoso <rian_santoso@ymail.com>
2019-06-27 09:27:09 +0700

    Commit 27 Juni 2019

--- application/views/ukurankebijakankeuangan.php
+++ application/views/ukurankebijakankeuangan.php
@@ -80,9 +80,6 @@
 		<!-- begin:: Header Mobile -->
 		<div id="kt_header_mobile" class="kt-header-mobile  kt-header-mobile--fixed ">
 			<div class="kt-header-mobile__logo">
-				<a href="index.html">
-					<img alt="Logo" src="<?php echo base_url();?>assets/media/logos/logo-light.png" />
-				</a>
 			</div>
 			<div class="kt-header-mobile__toolbar">
 				<button class="kt-header-mobile__toggler kt-header-mobile__toggler--left" id="kt_aside_mobile_toggler"><span></span></button>
